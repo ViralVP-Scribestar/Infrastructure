7caad8700b6a6b75d3ad2b2a3cacf74667f810a3
Edward Dewhurst <edward.dewhurst@scribestar.com>
2015-01-23 14:24:58 +0000

    updated error checking and logging in Create-VM.ps1

--- Scribestar-Infrastructure/Environment/Test/Create-VM.ps1
+++ Scribestar-Infrastructure/Environment/Test/Create-VM.ps1
@@ -1,7 +1,8 @@
 ﻿param([string] $Name, [string] $ServerType, [string] $IP, [string] $Gateway);
 
-if(-not (Get-PSSnapin VMware.VimAutomation.Core))
-{
+try {
+	Get-PSSnapin VMware.VimAutomation.Core
+} catch {
    Add-PSSnapin VMware.VimAutomation.Core
 }
 
@@ -18,12 +19,14 @@ $Role = Import-ScribestarRole $RoleFile
 $VMExists = Get-VM -Name $Name -ErrorAction SilentlyContinue
 
 if($VMExists.Name -eq $Name) {
-    Write-Host "ERROR: $Name Already exists"
+    Write-Host "ERROR: $Name Already exists" -ForegroundColor Red
 } else {
     Write-Host "Creating VM $Name with role $RoleFile" -ForegroundColor Green
 
     New-ScribestarVM -Name $Name -ResourcePool $Role.Resource -Datastore $Role.Datastore -Template $Role.Template -Location $Role.Location
     
+	Write-Host "Sleeping for 30s" -ForegroundColor Green
+
     Start-Sleep -Seconds 30
 
 	Set-ScribestarNetwork -VMName $Name -Network $Network
@@ -31,23 +34,33 @@ if($VMExists.Name -eq $Name) {
     # thin by default
     New-ScribestarHardDisk -Name $Name -CapacityGB $Role.CapacityGB
 
+	Write-Host "Starting VM and waiting 60s" -ForegroundColor Green
+
     Start-VM -VM $Name
 
     Start-Sleep -Seconds 60
+
+	Write-Host "Creating HDD" -ForegroundColor Green
 
     $DiskSetupScript = Get-ScribestarRemoteDiskSetupScript($Name)
 
     Invoke-VMScript -VM $Name -ScriptText $DiskSetupScript -ScriptType Powershell -GuestUser $GuestUser -GuestPassword $GuestPassword -HostUser $HostUser -HostPassword $HostPassword
 
-    Write-Host "Rebooting VM's" -ForegroundColor Green
+    Write-Host "Rebooting VM and waiting 120s" -ForegroundColor Green
 
     Restart-VMGuest -VM $Name -Confirm:$false
 
     Start-Sleep -Seconds 120
 
     $NetworkSetupScript = Get-ScribestarRemoteNetworkSetupScript($IP, $Gateway)
+	
+	Write-Host "Applying network settings" -ForegroundColor Green
+    
+	Invoke-VMScript -VM $Name -ScriptText $NetWorkSetupScript -ScriptType Powershell -GuestUser $GuestUser -GuestPassword $GuestPassword -HostUser $HostUser -HostPassword $HostPassword
 
-    Invoke-VMScript -VM $Name -ScriptText $NetWorkSetupScript -ScriptType Powershell -GuestUser $GuestUser -GuestPassword $GuestPassword -HostUser $HostUser -HostPassword $HostPassword
+	Write-Host "Rebooting" -ForegroundColor Green	
 
     Restart-VMGuest -VM $Name -Confirm:$false
+
+	Write-Host "Done!" -ForegroundColor Green
 }
