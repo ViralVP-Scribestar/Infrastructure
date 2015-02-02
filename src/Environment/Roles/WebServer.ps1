7ad24659f65183f36d30aef293740dc24325e398
grant drage <grant.drage@scribestar.com>
2015-02-02 14:33:28 +0000

    WebServer.ps1 Update

--- src/Environment/Roles/WebServer.ps1
+++ src/Environment/Roles/WebServer.ps1
@@ -2,24 +2,34 @@
 
 $file = ".\Data\WebServer-Role.xml"
 
-#If (Test-Path $fileName) {
-    #Invoke-Command -ComputerName $server -ScriptBlock     {
-    ##    Import-Module Servermanager
-     #   Get-Command -Module Servermanager
+Invoke-Command -ComputerName $server -ScriptBlock     {
+    $file = ".\Data\WebServer-Role.xml"
 
-        Write-Host -fore Green "Installing Web Features and Roles . $file . "
-        Install-WindowsFeature -ComputerName $server -ConfigurationFilePath $file 
+    #  1. Backup the Windows Update policy
+    Write-Host "1"
+    C:\Windows\system32\reg.exe export "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" c:\WUPol.reg
+    # 2. Remove it
+    Write-Host "2"
+    Remove-Item HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate -Recurse -Force
 
-#        Import-Clixml $file | Add-WindowsFeature
+    Write-Host "3"
+    Import-Module Servermanager
+    Write-Host "4"
+    Get-Service WUAUServ,BITS | Stop-Service -PassThru -Verbose |Start-Service -PassThru -Verbose
 
-        Write-Host "Installing Firewall Rules!"
-        #New-NetFirewallRule -DisplayName "Web Server Rule" -Direction Inbound -Protocol TCP -Action Allow -LocalPort 80, 443
-    #}
-#}
- #   Else {
-  #      Write-Host -fore Red "Unable to find Web features xml file!"
-   #     Write-Host -fore Red "$fileName does NOT exist!"
-    #    break;
-   # }
+    Write-Host "5"
+    Write-Host -fore Green "Installing Web Features and Roles . $file . "
+    Install-WindowsFeature -ConfigurationFilePath "C:\Users\gdrage\Documents\Data\SqlServer-Role.xml" -Source "Windows Update"
+
+    Write-Host "Installing Firewall Rules!"
+    New-NetFirewallRule -DisplayName "Web Server Rule" -Direction Inbound -Protocol TCP -Action Allow -LocalPort 80, 443
+
+    Write-Host "6"
+    #  3. Restore the Windows Update policy
+    regedit /s C:\WUPol.reg
+    Write-Host "7"
+    #  4. Remove Backup
+    Remove-Item C:\WUPol.reg -Force
+}
 
 Write-Host "All Done!"
