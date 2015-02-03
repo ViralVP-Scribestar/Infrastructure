c37e89cd211db685224c661e7cb6171636d871fd
grant drage <grant.drage@scribestar.com>
2015-02-03 14:17:48 +0000

    Update CreateAllVMs

--- src/Modules/CreateAllVM's.ps1
+++ src/Modules/CreateAllVM's.ps1
@@ -1,11 +1,22 @@
 ﻿.\Create-VM.ps1 -Name BETAWIN-APP01 -ServerConfigFile ..\Environment\Data\Alpha-Beta.csv
+..\Environment\Roles\AppServer.ps1 -server BETAWIN-APP01
 .\Create-VM.ps1 -Name BETAWIN-APP02 -ServerConfigFile ..\Environment\Data\Alpha-Beta.csv
+..\Environment\Roles\AppServer.ps1 -server BETAWIN-APP02
 .\Create-VM.ps1 -Name BETAWIN-WEB01 -ServerConfigFile ..\Environment\Data\Alpha-Beta.csv
+..\Environment\Roles\WebServer.ps1 -server BETAWIN-WEB01
 .\Create-VM.ps1 -Name BETAWIN-WEB02 -ServerConfigFile ..\Environment\Data\Alpha-Beta.csv
+..\Environment\Roles\WebServer.ps1 -server BETAWIN-WEB02
 .\Create-VM.ps1 -Name BETAWIN-MQ01 -ServerConfigFile ..\Environment\Data\Alpha-Beta.csv
+..\Environment\Roles\RabbitMQServer.ps1 -server BETAWIN-MQ01
 .\Create-VM.ps1 -Name BETAWIN-MQ02 -ServerConfigFile ..\Environment\Data\Alpha-Beta.csv
+..\Environment\Roles\RabbitMQServer.ps1 -server BETAWIN-MQ02
 .\Create-VM.ps1 -Name BETAWIN-RAVEN01 -ServerConfigFile ..\Environment\Data\Alpha-Beta.csv
+..\Environment\Roles\RavenDBServer.ps1 -server BETAWIN-RAVEN01
 .\Create-VM.ps1 -Name BETAWIN-RAVEN02 -ServerConfigFile ..\Environment\Data\Alpha-Beta.csv
+..\Environment\Roles\RavenDBServer.ps1 -server BETAWIN-RAVEN02
 .\Create-VM.ps1 -Name BETAWIN-NEO01 -ServerConfigFile ..\Environment\Data\Alpha-Beta.csv
+..\Environment\Roles\Neo4jServer.ps1 -server BETAWIN-NEO01
 .\Create-VM.ps1 -Name BETAWIN-NEO02 -ServerConfigFile ..\Environment\Data\Alpha-Beta.csv
+..\Environment\Roles\Neo4jServer.ps1 -server BETAWIN-NEO02
 .\Create-VM.ps1 -Name BETAWIN-SQL01 -ServerConfigFile ..\Environment\Data\Alpha-Beta.csv
+..\Environment\Roles\SqlServer.ps1 -server BETAWIN-SQL01
