500b737d20d072b14cac576c5aabee4c4b9303a2
grant drage <grant.drage@scribestar.com>
2015-02-03 10:22:17 +0000

    Roles and Feature WORKING

--- src/Environment/Roles/AppServer.ps1
+++ src/Environment/Roles/AppServer.ps1
@@ -1,38 +1,30 @@
-﻿﻿param([switch]$Elevated)
+﻿param([string]$server)
 
-Function Test-Admin {
-  $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
-  $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
-}
+$file = ".\Data\AppServer-Role.xml"
+Copy-Item -Path $file -Destination \\$server\c$\roles.xml;
 
-if ((Test-Admin) -eq $false)  {
-    if ($elevated) 
-    {
-        # tried to elevate, did not work, aborting
-    } 
-    else {
-        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
-}
+Invoke-Command -ComputerName $server -ScriptBlock     {
+    C:\Windows\system32\reg.exe export "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" c:\WUPol.reg
+    Remove-Item HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate -Recurse -Force
 
-exit
-}
-
-$fileName = ".\Data\AppServer-Role.xml"
-
-If (Test-Path $fileName) {
     Import-Module Servermanager
-    Get-Command -Module Servermanager
+    Get-Service WUAUServ,BITS | Stop-Service -PassThru -Verbose |Start-Service -PassThru -Verbose
 
     Write-Host -fore Green "Installing App Features and Roles"
-    Import-Clixml $fileName | Add-WindowsFeature
+    Import-Clixml C:\roles.xml | Install-WindowsFeature -Source "Windows Update"
 
     Write-Host "Installing Firewall Rules!"
     New-NetFirewallRule -DisplayName "App" -Direction Inbound -Protocol TCP -Action Allow -LocalPort 80, 443
+
+    sleep -Seconds 10
+
+    regedit /s C:\WUPol.reg
+    
+    Remove-Item C:\WUPol.reg -Force
+    Remove-Item C:\roles.xml -Force
+
+    Write-Host "Restarting Server!"
+    Restart-Computer -Force
 }
-    Else {
-        Write-Host -fore Red "Unable to find App features xml file!"
-        Write-Host -fore Red "$fileName does NOT exist!"
-        break;
-    }
 
 Write-Host "All Done!"
--- src/Environment/Roles/WebServer.ps1
+++ src/Environment/Roles/WebServer.ps1
@@ -4,9 +4,7 @@ $file = ".\Data\WebServer-Role.xml"
 Copy-Item -Path $file -Destination \\$server\c$\roles.xml;
 
 Invoke-Command -ComputerName $server -ScriptBlock     {
-    #  1. Backup the Windows Update policy
     C:\Windows\system32\reg.exe export "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" c:\WUPol.reg
-    # 2. Remove it
     Remove-Item HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate -Recurse -Force
 
     Import-Module Servermanager
@@ -18,14 +16,15 @@ Invoke-Command -ComputerName $server -ScriptBlock     {
     Write-Host "Installing Firewall Rules!"
     New-NetFirewallRule -DisplayName "Web Server Rule" -Direction Inbound -Protocol TCP -Action Allow -LocalPort 80, 443
 
-    #  3. Restore the Windows Update policy
+    sleep -Seconds 10
+
     regedit /s C:\WUPol.reg
     
     Remove-Item C:\WUPol.reg -Force
     Remove-Item C:\roles.xml -Force
 
-    Write-Host "Restarting $server!"
-    Restart-Computer
+    Write-Host "Restarting Server!"
+    Restart-Computer -Force
 }
 
 Write-Host "All Done!"
