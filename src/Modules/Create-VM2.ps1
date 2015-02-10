a81d8b242b66113d8889db41c9a3f003ca685eda
Viral Patel <viral.patel@scribestar.com>
2015-02-10 17:21:36 +0000

    VM

--- src/Modules/Create-VM2.ps1
+++ src/Modules/Create-VM2.ps1
@@ -16,17 +16,13 @@ $hostuser = "root"
 $hostpassword = "scribestar"
 ##############################
 
-#Find VM Required To Create
-[string]$value = "TRUE"
-[boolean]$a = $value
-[boolean]$a = [System.Convert]::ToBoolean($value)
-
-
 $Roles = Import-ScribestarServerCSV $ServerConfigFile | Where-Object {$_.Required -eq $true}
 
 foreach ($Role in $Roles) {
 
 $Name = $Role.Name
+$IPAddress = $Role.IPAddress
+$Gateway = $Role.Gateway
 
 $pattern= @"
 ^([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3}$
