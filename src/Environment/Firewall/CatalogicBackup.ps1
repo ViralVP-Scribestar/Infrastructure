84ee8b53cc7791e8d759a62225918ccd5cdf790a
Viral Patel <viral.patel@scribestar.com>
2015-02-12 10:55:57 +0000

    Catalogic Script

---
+++ src/Environment/Firewall/CatalogicBackup.ps1
@@ -0,0 +1,36 @@
+﻿param([string]$Name)
+##Installing Catalogic##
+
+##Check if the ports are open
+
+try {
+	Remove-Module Scribestar-Functions -ErrorAction SilentlyContinue
+} catch {}
+
+Import-Module ".\Modules\Scribestar-Functions.psm1"  -ErrorAction Stop
+
+$Ports = "6123","10000","10566"
+                            
+                            try{
+                            $Status = Test-ScribestarPort -Computer $Name -Port $Ports -TCP -ErrorAction SilentlyContinue
+                            $ServerName = $Status.Server
+                            }
+
+                            catch {
+                                    "The port is not open on this $ServerName"
+                                    }
+
+                                    if($status.Open -eq "True") {Write-Host "Ports Are Already Open"}
+
+                                    else {
+
+                                    
+                                    
+                                            Invoke-Command -ComputerName $Name -ScriptBlock {
+                                                                                                       
+                                                                                                   $Ports = "6123","10000","10566"
+                                                                                                   New-NetFirewallRule -DisplayName "Catalogic" -Direction Inbound -Protocol TCP -Action Allow -LocalPort $Ports
+                                          
+
+                                                                                     }
+                            }
